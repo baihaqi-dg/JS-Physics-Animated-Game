3020beadd941ce11f3d98977187b3f6ec638c8d4
Muhd Baihaqi <mbaihaqisukaini@gmail.com>
2023-02-18 22:35:00 +0800

    initial file

---
+++ src/script.js
@@ -0,0 +1,6 @@
+window.addEventListener('load',function(){
+    const canas = document.getElementById('canvas1');
+    const ctx = canvas.getContext('iamge/2d');
+    canvas.width = 1280;
+    canvas.height = 720; 
+})
