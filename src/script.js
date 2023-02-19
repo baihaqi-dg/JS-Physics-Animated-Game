6babef61d62fcba4031c04f01f97c5f6da1117f3
Muhd Baihaqi <mbaihaqisukaini@gmail.com>
2023-02-19 22:16:09 +0800

    sprite animation

--- src/script.js
+++ src/script.js
@@ -19,9 +19,21 @@ window.addEventListener('load',function(){
             this.speedY = 0;
             this.dx = 0;
             this.dy = 0;
-            this.speedModifier = 20;
+            this.speedModifier = 5;
+            this.spriteWidth = 255;
+            this.spriteHeight = 255;
+            this.width = this.spriteWidth;
+            this.height = this.spriteHeight;
+            this.spriteX;
+            this.spriteY;
+            this.frameX = 0;
+            this.frameY = 5;
+            this.image = document.getElementById('bull');
         }
         draw(context){
+            context.drawImage(this.image, this.frameX * this.spriteWidth,
+                this.frameY * this.spriteHeight, this.spriteWidth, this.spriteHeight,
+                this.spriteX,this.spriteY,this.width,this.height);
             context.beginPath();
             context.arc(this.collisionX,this.collisionY,this.collisionRadius,0,Math.PI * 2);
             context.save();
@@ -35,6 +47,19 @@ window.addEventListener('load',function(){
             context.stroke()
         }
         update(){
+            //sprite animation
+            this.dx = this.game.mouse.x - this.collisionX;
+            this.dy = this.game.mouse.y - this.collisionY;
+            const angle = Math.atan2(this.dy,this.dx);
+            if (angle<-2.74 || angle >2.74) this.frameY = 6 
+            else if(angle<-1.96) this.frameY = 7;
+            else if(angle<-1.17) this.frameY = 0;
+            else if(angle<-0.39) this.frameY = 1;
+            else if(angle<0.39) this.frameY = 2;
+            else if(angle<1.17) this.frameY = 3;
+            else if(angle<1.96) this.frameY = 4;
+            else if(angle<2.74) this.frameY = 5;
+
             this.dx = this.game.mouse.x - this.collisionX;
             this.dy = this.game.mouse.y - this.collisionY;
             const distance = Math.hypot(this.dy,this.dx);
@@ -47,6 +72,8 @@ window.addEventListener('load',function(){
             }
             this.collisionX += this.speedX * this.speedModifier;
             this.collisionY += this.speedY * this.speedModifier;
+            this.spriteX = this.collisionX - this.width * 0.5;
+            this.spriteY = this.collisionY - this.height * 0.5;
             //check collison
             this.game.obstacle.forEach(obstacles =>{
                 // if(this.game.checkCollision(this,obstacles)){
